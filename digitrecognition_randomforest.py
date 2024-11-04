580293428b6ba0848ed0572a5e7c7a8fc06689ba
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-11-04 18:52:08 +0530

    3

--- digitrecognition_randomforest.py
+++ digitrecognition_randomforest.py
@@ -22,56 +22,63 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 ##model Training
 from sklearn.ensemble import RandomForestClassifier
 clf = RandomForestClassifier(n_estimators=100)
-clf.fit(X_train, y_train)
+model=clf.fit(X_train, y_train)
 
 ##model prediction
-y_pred = clf.predict(X_test)
+y_pred = model.predict(X_test)
 
-print(clf.score(X_test, y_test))
+print(np.column_stack((y_test, y_pred)))
+
+print(model.score(X_test, y_test))
 
 ##accuracy
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:{0}".format(accuracy*100))
 
+import matplotlib.pyplot as plt
+index=int(input("Enter the index of the digit you want to predict: "))
+print("Predicted digit is:",str(model.predict([X_test.iloc[index]])))
+plt.imshow(X_test.iloc[index].values.reshape((28,28)),cmap='gray')
 
 
-import cv2  
 
-def resize_image(image_path, num_rows, num_cols):  
-    # Read the image  
-    image = cv2.imread(image_path)  
+# import cv2  
+
+# def resize_image(image_path, num_rows, num_cols):  
+#     # Read the image  
+#     image = cv2.imread(image_path)  
     
-    # Get current dimensions  
-    height, width = image.shape[:2]  
+#     # Get current dimensions  
+#     height, width = image.shape[:2]  
     
-    # Calculate the aspect ratio  
-    aspect_ratio = width / height  
+#     # Calculate the aspect ratio  
+#     aspect_ratio = width / height  
     
-    # Compute the new dimensions  
-    new_width = int(num_cols * (height / num_rows) * aspect_ratio)  
-    new_height = int(num_rows * (width / num_cols) / aspect_ratio)  
+#     # Compute the new dimensions  
+#     new_width = int(num_cols * (height / num_rows) * aspect_ratio)  
+#     new_height = int(num_rows * (width / num_cols) / aspect_ratio)  
     
-    # Resize the image  
-    resized_image = cv2.resize(image, (new_width, new_height))  
+#     # Resize the image  
+#     resized_image = cv2.resize(image, (new_width, new_height))  
     
-    return  resized_image
+#     return  resized_image
 
-resize_image('images.jpg', 28, 28)
+# resize_image('images.jpg', 28, 28)
 
-from PIL import Image  
+# from PIL import Image  
 
-# Load the image  
-img = Image.open('resized_image.jpg')
+# # Load the image  
+# img = Image.open('resized_image.jpg')
 
-# Convert image to grayscale (optional, if you need grayscale values)  
-img = img.convert('L')  
+# # Convert image to grayscale (optional, if you need grayscale values)  
+# img = img.convert('L')  
 
-# Get pixel values  
-pixel_values = list(img.getdata())  
+# # Get pixel values  
+# pixel_values = list(img.getdata())  
 
-# Print pixel values  
-# print(pixel_values)
+# # Print pixel values  
+# # print(pixel_values)
 
-y_pred1 = clf.predict((pixel_values))
-print(y_pred1)
+# y_pred1 = clf.predict((pixel_values))
+# print(y_pred1)
